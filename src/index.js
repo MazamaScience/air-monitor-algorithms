a736d2ef5f856f536ccd733a7ac7b2911ca38e0c
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2025-06-18 12:30:41 -0700

    added comments to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 /*
- * Re-export all functions defined in individual files.
+ * air-monitor-algorithms v1.0.4
+ * Main module entry point. Exports all algorithm and utility functions.
+ *
+ * Example:
+ * import { dailyStats } from 'air-monitor-algorithms';
  */
 
+// High-level algorithms
 export { dailyStats } from "./dailyStats.js";
 export { diurnalStats } from "./diurnalStats.js";
 export { pm_nowcast } from "./nowcast.js";
+
+
+// Date utilities
 export { trimDate } from "./trimDate.js";
+
+// Array utilities
 export {
   arrayMin,
   arrayMax,
